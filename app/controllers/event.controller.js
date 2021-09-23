d081d6475718175604f82d5880258caacf376020
sachin <sachin.waghmare@standardbank.co.za>
2021-09-23 22:57:52 +0530

    added filtering for event

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -6,7 +6,6 @@ const User = db.user;
 const EmailController = require("./email");
 const utils = require("../utils/functions");
 const { COMMON, EVENT } = require("../utils/constants");
-
 exports.createupdateEvent = async (req, res) => {
   try {
     const { userId } = req;
@@ -99,15 +98,36 @@ exports.createupdateEvent = async (req, res) => {
 
 exports.getEvents = async (req, res) => {
   try {
-    const { societyId, page = 1, limit = 10 } = req.query;
+    const { societyId, page = 1, limit = 10, filterType } = req.query;
     const options = {
       page: page,
       limit: limit,
+      filterType: filterType,
       sort: { created_at: -1 },
     };
     let query = {};
     if (societyId) {
       query.societyId = societyId;
+    }
+    if (filterType) {
+      let startOffToday = new Date();
+      let today = new Date(
+        startOffToday.getFullYear(),
+        startOffToday.getMonth(),
+        startOffToday.getDate()
+      );
+      let tomorrow = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 1
+      );
+      if (filterType === "todays") {
+        query.fromDateTime = { $gte: today, $lte: tomorrow };
+      } else if (filterType === "past") {
+        query.fromDateTime = { $lt: today };
+      } else if (filterType === "upcoming") {
+        query.fromDateTime = { $gte: tomorrow };
+      }
     }
 
     Event.paginate(query, options, function (err, result) {
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,111 +1,78 @@
 module.exports = {
+  SETTINGS: {
+    ANNOUNCEMENT: {
+      LATEST_DAYS: 7,
+      PASTS_DAYS: 7,
+    },
+    DEFAULT_PAGE_LIMIT: 10,
+  },
   AUTH: {
     SIGNUP: {
       CAN_NOT_CREATE_SOCIETY: "Can't able to create society.",
       USER_REGISTERED: "User register successfully.",
       EMAIL_ALREADY_EXIST: "Oops! Email is already in use!",
-      SETTINGS: {
-        ANNOUNCEMENT: {
-          LATEST_DAYS: 7,
-          PASTS_DAYS: 7,
-        },
-        DEFAULT_PAGE_LIMIT: 10,
-      },
-      AUTH: {
-        SIGNUP: {
-          CAN_NOT_CREATE_SOCIETY: "Can't able to create society.",
-          USER_REGISTERED: "User register successfully.",
-          EMAIL_ALREADY_EXIST: "Oops! Email is already in use!",
-        },
-        SIGNIN: {
-          SOCIETY_NOT_FOUND: "Society data not found.",
-          USER_NOT_FOUND: "User not found.",
-          INVALID_PASSWORD: "Invalid Password!",
-          NOT_CONFIRMED:
-            "Invalid password, You are not verified yet by society admin. Contact society admin for more details.",
-          INACTIVE:
-            "You account is inactive. Contact society admin for more details.",
-        },
-        NO_TOKEN: "No token provided!",
-        NOT_AUTHORIZED: "You are not authorized to use our service!",
-      },
-      SIGNIN: {
-        SOCIETY_NOT_FOUND: "Society data not found.",
-        USER_NOT_FOUND: "User not found.",
-        INVALID_PASSWORD: "Invalid Password!",
-        NOT_CONFIRMED:
-          "Invalid password, You are not verified yet by society admin. Contact society admin for more details.",
-        INACTIVE:
-          "You account is inactive. Contact society admin for more details.",
-      },
-      NO_TOKEN: "No token provided!",
-      NOT_AUTHORIZED: "You are not authorized to use our service!",
     },
-    PUBLIC: {
-      ALL_SOCIETY_DATA: "Society Data.",
-      SOCIETY_DATA_NOT_AVAILABLE: "Society Data not available.",
+    SIGNIN: {
+      SOCIETY_NOT_FOUND: "Society data not found.",
+      USER_NOT_FOUND: "User not found.",
+      INVALID_PASSWORD: "Invalid Password!",
+      NOT_CONFIRMED:
+        "Invalid password, You are not verified yet by society admin. Contact society admin for more details.",
+      INACTIVE:
+        "You account is inactive. Contact society admin for more details.",
     },
-    COMMON: {
-      SOMETHING_WRONG: "Something want wrong please try again.",
+    NO_TOKEN: "No token provided!",
+    NOT_AUTHORIZED: "You are not authorized to use our service!",
+  },
+  PUBLIC: {
+    ALL_SOCIETY_DATA: "Society Data.",
+    SOCIETY_DATA_NOT_AVAILABLE: "Society Data not available.",
+  },
+  COMMON: {
+    SOMETHING_WRONG: "Something want wrong please try again.",
+  },
+  EMAIL: {
+    PROVIDE_VALID_TEMPLATE: "Please provide a valid template name.",
+  },
+  ANNOUNCEMENT: {
+    TITLE_REQUIRED: "Announcement title is required!",
+    TITLE_NOT_EMPTY: "Announcement title can't be empty!",
+    DESC_REQUIRED: "Announcement description is required!",
+    DESC_NOT_EMPTY: "Announcement description can't be empty!",
+    UPDATED: "Announcement updated successfully.",
+    CREATED: "Announcement created successfully.",
+    DELETED: "Announcement deleted successfully.",
+    ALL: "All announcements.",
+    NOT_FOUND: "No announcement found.",
+  },
+  COMPLAINT: {
+    TITLE_REQUIRED: "Complaint title is required!",
+    TITLE_NOT_EMPTY: "Complaint title can't be empty!",
+    DESC_REQUIRED: "Complaint description is required!",
+    DESC_NOT_EMPTY: "Complaint description can't be empty!",
+    UPDATED: "Complaint updated successfully.",
+    CREATED: "Complaint created successfully.",
+    DELETED: "Complaint deleted successfully.",
+    ALL: "All complaints.",
+    NOT_FOUND: "No complaint found.",
+  },
+  EVENT: {
+    VALIDATION: {
+      TITLE_REQUIRED: "Event title is required!",
+      TITLE_NOT_EMPTY: "Event title can't be empty!",
+      DESC_REQUIRED: "Event description is required!",
+      DESC_NOT_EMPTY: "Event description can't be empty!",
+      FROM_DATE_TIME: "Event fromdatetime is required",
+      TO_DATE_TIME: "Event todateTime is required",
+      IMG: "Event image is required",
+      VENUE: "Event venue is required",
     },
-    EMAIL: {
-      PROVIDE_VALID_TEMPLATE: "Please provide a valid template name.",
-    },
-    ANNOUNCEMENT: {
-      TITLE_REQUIRED: "Announcement title is required!",
-      TITLE_NOT_EMPTY: "Announcement title can't be empty!",
-      DESC_REQUIRED: "Announcement description is required!",
-      DESC_NOT_EMPTY: "Announcement description can't be empty!",
-      UPDATED: "Announcement updated successfully.",
-      CREATED: "Announcement created successfully.",
-      DELETED: "Announcement deleted successfully.",
-      ALL: "All announcement.",
-      NOT_FOUND: "No announcement found.",
-    },
-
-    EVENT: {
-      VALIDATION: {
-        TITLE_REQUIRED: "Event title is required!",
-        TITLE_NOT_EMPTY: "Event title can't be empty!",
-        DESC_REQUIRED: "Event description is required!",
-        DESC_NOT_EMPTY: "Event description can't be empty!",
-        FROM_DATE_TIME: "Event fromdatetime is required",
-        TO_DATE_TIME: "Event todateTime is required",
-        IMG: "Event image is required",
-        VENUE: "Event venue is required",
-      },
-      RESPONSE: {
-        UPDATED: "Event updated successfully.",
-        CREATED: "Event created successfully.",
-        DELETED: "Event deleted successfully.",
-        ALL: "All events.",
-        NOT_FOUND: "No event found.",
-      },
-    },
-    EMAIL: {
-      PROVIDE_VALID_TEMPLATE: "Please provide a valid template name.",
-    },
-    ANNOUNCEMENT: {
-      TITLE_REQUIRED: "Announcement title is required!",
-      TITLE_NOT_EMPTY: "Announcement title can't be empty!",
-      DESC_REQUIRED: "Announcement description is required!",
-      DESC_NOT_EMPTY: "Announcement description can't be empty!",
-      UPDATED: "Announcement updated successfully.",
-      CREATED: "Announcement created successfully.",
-      DELETED: "Announcement deleted successfully.",
-      ALL: "All announcements.",
-      NOT_FOUND: "No announcement found.",
-    },
-    COMPLAINT: {
-      TITLE_REQUIRED: "Complaint title is required!",
-      TITLE_NOT_EMPTY: "Complaint title can't be empty!",
-      DESC_REQUIRED: "Complaint description is required!",
-      DESC_NOT_EMPTY: "Complaint description can't be empty!",
-      UPDATED: "Complaint updated successfully.",
-      CREATED: "Complaint created successfully.",
-      DELETED: "Complaint deleted successfully.",
-      ALL: "All complaints.",
-      NOT_FOUND: "No complaint found.",
+    RESPONSE: {
+      UPDATED: "Event updated successfully.",
+      CREATED: "Event created successfully.",
+      DELETED: "Event deleted successfully.",
+      ALL: "All events.",
+      NOT_FOUND: "No event found.",
     },
   },
 };
